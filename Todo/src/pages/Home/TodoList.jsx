ebef945c6f11e74f0a9e88a9d336a894c94a3dde
Kaleb Moore <k4l3bmoore@gmail.com>
2022-09-03 12:21:11 -0600

    Update Todos show to screen
    Todos now show on screen and will add the next todo to the bottom
    of the list. The plan is to change the order only when a Todo is updated.
    If the todo is marked as complete it should be shifted to the bottom,
    top of the stack at the bottom should be the most recent Todo. Will
    be adding a filter as well to show only completed Todos.

--- Todo/src/pages/Home/TodoList.jsx
+++ Todo/src/pages/Home/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from '../Todo';
 import TodoForm from './TodoForm'
 
 export default function TodoList() {
@@ -14,12 +15,26 @@ export default function TodoList() {
         const newTodos = [...todos, todo];
 
         setTodos(newTodos);
+    };
+
+    function completeTodo(id) {
+        let updateTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            };
+            return todo;
+        });
+        setTodos(updateTodos);
     }
 
     return (
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo 
+                todos={todos}
+                completeTodo={completeTodo}
+            />
         </div>
     )
-}
+};
