27f950dde0e27939c3d5f2c8a3e3a6cb8346d3a0
Kaleb Moore <k4l3bmoore@gmail.com>
2022-09-03 11:49:27 -0600

    Create basic TodoForm
    Created button component for easy use if more buttons are needed. Currently just set the input
    to set the value to "" upon clicking the button. Will be changing this later and have commented
    out the code I plan on using.

---
+++ Todo/src/pages/Home/TodoForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import {nanoid} from 'nanoid'
+import Button from '../../components/form/Button.jsx'
+
+export default function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  function handleChange(e) {
+    setInput(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // props.onSubmit({
+    //     id: nanoid(),
+    //     text: input
+    // });
+
+    setInput('')
+  }
+
+  return (
+    <div>
+      <form
+        action=""
+        className="todo-form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={input}
+          name="text"
+          className="todo-input"
+          onChange={handleChange}
+        />
+        <Button name="Add todo" />
+      </form>
+    </div>
+  );
+}
---
+++ Todo/src/pages/Home/TodoList.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+export default function TodoList() {
+
+    const [todos, setTodos] = useState([])
+
+    return (
+        <div>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm />
+        </div>
+    )
+}
